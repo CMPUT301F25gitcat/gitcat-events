ba27c1cd7d422baac6b7c8b2c092c54183853a05
Andriy Peleshchyshyn <andriyponline@gmail.com>
2025-10-30 19:07:54 -0600

    added nav graph, need to integrate with ProfileActivity

--- code/app/src/main/java/com/example/gitcat_events/MainActivity.java
+++ code/app/src/main/java/com/example/gitcat_events/MainActivity.java
@@ -8,16 +8,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.gitcat_events.databinding.ActivityMainBinding;
 import com.example.gitcat_events.features.entrant.ui.ProfileActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startActivity(new Intent(this, ProfileActivity.class));
         finish(); // so back button doesn’t return to Hello World
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
