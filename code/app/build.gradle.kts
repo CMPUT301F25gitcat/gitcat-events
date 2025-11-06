ffa7f77362bd3faad1af742eca30c0748d7a6aba
fsoehn <fsoehn@ualberta.ca>
2025-11-05 19:46:47 -0700

    added homepage and event cards

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
