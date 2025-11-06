90b3bcf2db7897cbfcdbd169bddcc6476470a7ec
fsoehn <108768031+fsoehn@users.noreply.github.com>
2025-11-05 19:49:42 -0700

    added homepage and event cards (#51)

---
+++ code/app/src/main/java/com/example/gitcat_events/features/event/ui/EventArrayAdapter.java
@@ -0,0 +1,44 @@
+package com.example.gitcat_events.features.event.ui;
+
+import com.example.gitcat_events.R;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.gitcat_events.core.model.Event;
+
+import java.util.ArrayList;
+
+public class EventArrayAdapter extends ArrayAdapter<Event> {
+    public EventArrayAdapter(Context context, ArrayList<Event> events) {
+        super(context, 0, events);
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.event_list_item, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        Event event = getItem(position);
+
+        TextView eventName = view.findViewById(R.id.eventCardTitleText);
+        TextView eventDescription = view.findViewById(R.id.eventCardSubtitleText);
+        TextView eventDate = view.findViewById((R.id.eventCardDateText));
+
+        eventName.setText(event.getName());
+        eventDescription.setText(event.getDesc());
+        eventDate.setText(event.getDate());
+
+        return view;
+    }
+}
