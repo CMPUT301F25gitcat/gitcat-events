d35c58229e807b000b7cc318179175e97ef1f6c8
Ryan <ryan.chattopadhyay1@gmail.com>
2025-11-06 23:55:03 -0700

    bug: small dataTypeError fix

--- code/app/src/main/java/com/example/gitcat_events/core/model/AcceptedListEntry.java
+++ code/app/src/main/java/com/example/gitcat_events/core/model/AcceptedListEntry.java
@@ -1,7 +1,14 @@
 package com.example.gitcat_events.core.model;
 
 public class AcceptedListEntry extends Entry {
-    public AcceptedListEntry(int EventID, int UserID){
-        super(EventID, UserID);
+    //TODO: javadocs, tests
+    
+    // No-arg constructor for Firebase
+    public AcceptedListEntry() {
+        super();
+    }
+    
+    public AcceptedListEntry(String eventId, String userDeviceId){
+        super(eventId, userDeviceId);
     }
 }
