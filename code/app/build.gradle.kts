9be0e0da6ee7785496d2b237d6cdd723c78f5728
fsoehn <108768031+fsoehn@users.noreply.github.com>
2025-11-06 23:58:02 -0700

    feat: added real data to homepage (#62)

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.cardview)
-    
+
     // Glide for image loading
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
