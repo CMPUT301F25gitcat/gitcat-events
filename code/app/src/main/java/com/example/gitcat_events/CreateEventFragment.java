b3191f427e81573571d0b5a7eb35e6d22fae6e50
Momoore Oshinaike <momo.mts06@gmail.com>
2025-11-06 22:37:27 -0700

    feat: added base of createEvent

--- code/app/src/main/java/com/example/gitcat_events/CreateEventFragment.java
+++ code/app/src/main/java/com/example/gitcat_events/CreateEventFragment.java
@@ -1,6 +1,14 @@
 package com.example.gitcat_events;
 
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
+
+import com.example.gitcat_events.core.model.Profile;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Date;
 
@@ -14,6 +22,13 @@ public class CreateEventFragment extends Fragment {
     private String poster;
     private Boolean geoLocationRequired;
     private int organizer;
-
+    private static final String TAG = "CreateEventFragment";
+    private static final String PREFS = "app_prefs";
+    private static final String KEY_PROFILE_ID = "profile_doc_id";
+    private FirebaseFirestore db;
+    private ImageView imgPoster;
+    private Button btnEditPoster, btnEventTime, btnRaffleTime, btnCreateEvent, btnCancel;
+    private TextInputEditText inpEventName, inpDesc, inpSlotsAvailable;
+    private Profile currentProfile;
 
 }
