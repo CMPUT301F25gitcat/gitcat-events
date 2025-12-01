1e13d56a16ef2888cf1c77f543ac5b3a1fa898c9
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-12-01 04:50:10 -0700

    chore: importing view class

--- code/app/src/main/java/com/example/gitcat_events/AcceptedEntrantsMapActivity.java
+++ code/app/src/main/java/com/example/gitcat_events/AcceptedEntrantsMapActivity.java
@@ -2,6 +2,7 @@ package com.example.gitcat_events;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
