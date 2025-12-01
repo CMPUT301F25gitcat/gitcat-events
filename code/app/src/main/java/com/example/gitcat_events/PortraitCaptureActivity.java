d76b52bcebf1bd9dcb95343408eeacd370a94931
tridha643 <tridhatriv@gmail.com>
2025-12-01 13:01:14 -0700

    basically setting the potrait stuff to vertical mode only for mobile view.

---
+++ code/app/src/main/java/com/example/gitcat_events/PortraitCaptureActivity.java
@@ -0,0 +1,30 @@
+package com.example.gitcat_events;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * this is basically a custom capture activity which locks the orientation to portrait mode 
+ * 
+ */
+
+public class PortraitCaptureActivity extends CaptureActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
+
+
+// public class PortraitCaptureActivity extends CaptureActivity {
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         // Lock orientation to portrait
+//         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//     }
+// }
